2900c4a01be02feb385d0d6850adcddb96617e97
Thalles Ian <thallesyam@gmail.com>
2024-03-14 09:40:12 -0300

    feat: exercises module 02

---
+++ src/minimum/index.js
@@ -0,0 +1,22 @@
+// My solution
+// function minimum(first, second) {
+//   if(first < second) return first
+//   if(first > second) return second
+
+//   return `${first} isEqual ${second}`
+// }
+
+// console.log(minimum(1, 10))
+// console.log(minimum(111, 10))
+// console.log(minimum(10, 10))
+
+// Their solution
+// function min(a, b) {
+//   if (a < b) return a;
+//   else return b;
+// }
+
+// console.log(min(0, 10));
+// // → 0
+// console.log(min(0, -10));
+// // → -10
